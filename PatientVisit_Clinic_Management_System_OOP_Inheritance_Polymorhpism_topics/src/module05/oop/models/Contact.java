e53f47194560be757504d179fbe2b82697d49bbf
iotbands1train <137959267+iotbands1train@users.noreply.github.com>
2023-08-27 12:03:57 -0500

    Add files via upload
    update contact

--- PatientVisit_Clinic_Management_System_OOP_Inheritance_Polymorhpism_topics/src/module05/oop/models/Contact.java
+++ PatientVisit_Clinic_Management_System_OOP_Inheritance_Polymorhpism_topics/src/module05/oop/models/Contact.java
@@ -1,34 +1,56 @@
 package module05.oop.models;
- 
+
 import module05.oop.randompms.MyRandomPersonData;
 
+/**
+ * A class representing the contact information of a person.
+ */
 public class Contact {
-	
-	private Long phone;
+    private Long phone;
 
-	public Contact() {
-		super();
-		this.phone = Math.abs( MyRandomPersonData.random(214_111_1111L, 214_999_9999L));
-	}
+    /**
+     * Default constructor that initializes the contact with a random phone number.
+     */
+    public Contact() {
+        super();
+        this.phone = Math.abs(MyRandomPersonData.random(214_111_1111L, 214_999_9999L));
+    }
 
-	public Contact(Long phone) {
-		super();
-		this.phone = phone;
-	}
+    /**
+     * Constructor that allows specifying a custom phone number.
+     *
+     * @param phone The phone number to set for the contact.
+     */
+    public Contact(Long phone) {
+        super();
+        this.phone = phone;
+    }
 
-	public Long getPhone() {
-		return phone;
-	}
+    /**
+     * Get the phone number of the contact.
+     *
+     * @return The phone number.
+     */
+    public Long getPhone() {
+        return phone;
+    }
 
-	public void setPhone(Long phone) {
-		this.phone = phone;
-	}
+    /**
+     * Set the phone number of the contact.
+     *
+     * @param phone The phone number to set.
+     */
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
 
-	@Override
-	public String toString() {
-		return " Contact [phone=" + phone + "]";
-	}
-	
-	
-
+    /**
+     * Get a string representation of the contact's information.
+     *
+     * @return A string containing the contact's phone number.
+     */
+    @Override
+    public String toString() {
+        return "Contact [phone=" + phone + "]";
+    }
 }
